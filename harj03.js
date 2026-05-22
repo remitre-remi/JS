b6f9282fef35da9789d7f52daba984a03ef69979
remitre-remi <remi.treier@gmail.com>
2026-05-22 12:33:49 +0300

    jess

--- harj03.js
+++ harj03.js
@@ -7,3 +7,34 @@
 //      toote koguhind
 //      muudab toote kogust
 //      kuvab objekti sisu, kasutades sõne malli
+
+let toode = { 
+    nimetus: "sai", 
+    hind: 3, 
+    kogus: 2022,
+
+    koguhind: function () {
+        return this.hind * this.kogus;
+    }
+
+};
+
+const ostukorv = {
+tooted: [
+        { nimi:'Piim', hind:3.60, kogus:2 },
+        { nimi:'Leib', hind:2.00, kogus:1 },
+        { nimi:'Munad', hind:1.50, kogus:6 },
+        { nimi:'Juust', hind:4.20, kogus:1 },
+        { nimi:'Tomatid', hind:2.30, kogus:3 },
+    ],
+
+    koik_tooted: function () {
+        this.tooted.forEach(t => {
+        console.log(t.nimi + " " + t.hind + "€" + " " + t.kogus + "tk")
+        })
+    }
+}
+
+ostukorv.koik_tooted()
+    
+
